c910e8bb54c05511b3592de16d117e33b9326cba
Mateo Senoner <mateosenoner@gmail.com>
2018-05-15 20:30:07 +0200

    Etappe fertiggestellt

--- 27/biketirol.js
+++ 27/biketirol.js
@@ -1,17 +1,4 @@
-/*
-    Vorbereitung: GPX Track herunterladen und nach GeoJSON konvertieren
-    -------------------------------------------------------------------
-    Datenquelle https://www.data.gv.at/suche/?search-term=bike+trail+tirol&searchIn=catalog
-    Download Einzeletappen / Zur Ressource ...
-    Alle Dateien im unterverzeichnis data/ ablegen
-    Die .gpx Datei der eigenen Etappe als etappe00.gpx speichern
-    Die .gpx Datei über https://mapbox.github.io/togeojson/ in .geojson umwandeln und als etappe00.geojson speichern
-    Die etappe00.geojson Datei in ein Javascript Objekt umwandeln und als etappe00.geojson.js speichern
 
-    -> statt 00 natürlich die eigene Etappe (z.B. 01,02, ...25)
-*/
-
-// eine neue Leaflet Karte definieren
 let myMap = L.map("map");
 let markerGroup = L.featureGroup();
 let routeGroup= L.featureGroup().addTo(myMap);
@@ -45,30 +32,19 @@ let myLayers = {
             attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>"
         }
 	),
-   
-   
-    
-
 };
 
-myMap.addLayer(myLayers.geolandbasemap); //http://leafletjs.com/reference-1.3.0.html#map-addlayer
+myMap.addLayer(myLayers.geolandbasemap);
 
-let myMapControl = L.control.layers({ //http://leafletjs.com/reference-1.3.0.html#control-layers
+let myMapControl = L.control.layers({
     "Openstreetmap" : myLayers.osm,
     "Basemap" : myLayers.geolandbasemap,
 	"Sommerkarte" : myLayers.sommer,
 	"Winterkarte" : myLayers.winter,   
-	"Orthofoto" : myLayers.ortho      
-   
-    
-},{
-   
+	"Orthofoto" : myLayers.ortho          
+},{  
     "Start-Ziel" : markerGroup,
 	"Route" : routeGroup
-    
-
-
-
 },{
 	collapsed: false 
 });
@@ -99,17 +75,3 @@ let geojson = L.geoJSON(route).addTo(routeGroup);
 
 
 myMap.fitBounds(routeGroup.getBounds());
-
-// Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
-// WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
-
-// Maßstab metrisch ohne inch
-
-// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
-
-// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
-// Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
-
-// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
-
-// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
